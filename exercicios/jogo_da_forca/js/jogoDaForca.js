0a67d35e85210aca1359b08e1399f0553ede2e00
Fabio <pintofabio100@gmail.com>
2020-09-22 22:08:37 -0300

    Update jogoDaForca.js

--- exercicios/jogo_da_forca/js/jogoDaForca.js
+++ exercicios/jogo_da_forca/js/jogoDaForca.js
@@ -1,33 +1,29 @@
-function sorteioPalavra ()
-{
-    var sorteio = Math.floor(Math.random()*10);
-}
+function palavraSorteada()
+{  
+    var palavras = ["banana",
+                    "cavalo", 
+                    "ferrari", 
+                    "carregador", 
+                    "teclado", 
+                    "basquete",
+                    "programador", 
+                    "campeonato", 
+                    "coelho", 
+                    "ornitorrinco"];
+    
+    var dicas = ["fruta", 
+                 "animal grande", 
+                 "marca de carro",
+                 "acessório para celular",
+                 "periférico de computador",
+                 "esporte",
+                 "profissão",
+                 "tipo de competição",
+                 "animal pequeno",
+                 "animal aquático"]
 
-var palavra = ["banana", "cavalo", "ferrari", "carregador", "mongolia", "basquete","catapora", "campeonato", "coelho", "ornitorrinco"];
 
-function palavraSorteada(sorteio)
-{
-    switch (sorteio)
-    {
-        case 0: palavra = "banana";
-            break;
-        case 1: palavra = "cavalo";
-            break;
-        case 2: palavra = "ferrari";
-            break;
-        case 3: palavra = "carregador";
-            break;
-        case 4: palavra = "mongolia";
-            break;
-        case 5: palavra = "basquete";
-            break;
-        case 6: palavra = "catapora";
-            break;
-        case 7: palavra = "campeonato";
-            break;
-        case 8: palavra = "coelh";
-            break;
-        case 9: palavra = "ornitorrinco";
-            break;
-    }
+    var sorteio = Math.floor(Math.random()*palavras.length);
+    return palavras[sorteio]
+    
 }
