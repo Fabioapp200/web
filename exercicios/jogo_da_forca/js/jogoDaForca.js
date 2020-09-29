d93ce31a2e52efbc417c3e3f73b90e404d46dc38
Fabio <pintofabio100@gmail.com>
2020-09-29 12:18:58 -0300

    Jogo finalizado sem dicas

--- exercicios/jogo_da_forca/js/jogoDaForca.js
+++ exercicios/jogo_da_forca/js/jogoDaForca.js
@@ -4,9 +4,12 @@ var letra;
 var palavraFoiSorteada;
 var palavraDisplay;
 var contagemDeErros = 0;
+var fotoForca;
 
 
 function sortearPalavra() {
+    /* #region  Setup Inicial */
+    document.getElementById("botaoSorteio").innerText = "Sortear Palavra";
     var palavras = ["banana",
         "cavalo",
         "ferrari",
@@ -35,55 +38,82 @@ function sortearPalavra() {
     palavraAtual = palavras[sorteio];
     dicaAtual = dicas[sorteio];
     palavraFoiSorteada = 1;
+    contagemDeErros = 0;
+    document.getElementById("imagemDaForca").src = "./data/images/Forca-0.png";
 
     document.getElementById("palavraSorteadaText").innerText = "";
-    
+
     palavraDisplay = palavraAtual;
 
-    for (i = 0; i<palavraAtual.length; i++)
-    {
+    for (i = 0; i < palavraAtual.length; i++) {
         document.getElementById("palavraSorteadaText").innerText = document.getElementById("palavraSorteadaText").innerText + "_";
-        palavraDisplay = palavraDisplay.replace(palavraDisplay[i],"_")
+        palavraDisplay = palavraDisplay.replace(palavraDisplay[i], "_")
     }
     console.log(palavraAtual);
 
+    document.getElementById("botaoSorteio").disabled = true;
+    /* #endregion */
+
 }
 
- function tentarLetra(letra) 
-{
-    if(palavraFoiSorteada)
-    {
-        for (i = 0; i < palavraAtual.length; i++) 
-        {
+function tentarLetra(letra) {
+    if (palavraFoiSorteada) {
+        for (i = 0; i < palavraAtual.length; i++) {
             letraAtual = palavraAtual[i];
             letra = letra.toLowerCase();
+
             
-            if (letraAtual == letra)
-            {                
+            
+            if (letraAtual == letra) {
                 palavraDisplay = palavraDisplay.split("");
-                
+
                 palavraDisplay[i] = letra;
 
                 palavraDisplay = palavraDisplay.join("");
 
+
                 document.getElementById("palavraSorteadaText").innerText = palavraDisplay;
+
+
             }
-            if (!palavraAtual.includes(letra))
-            {
+            if(palavraDisplay == palavraAtual){
+                document.getElementById("imagemDaForca").src = "./data/images/Forca-6.png";
+                document.getElementById("botaoSorteio").innerText = "Jogar Novamente";
+                document.getElementById("botaoSorteio").disabled = false;
+            }
+            if (!palavraAtual.includes(letra)) {
                 contagemDeErros++;
                 console.log(contagemDeErros);
-                if (contagemDeErros == 5)
-                {
-                    document.getElementById("palavraSorteadaText").innerText = "Você perdeu";
+                switch (contagemDeErros) {
+                    case 0:
+                        document.getElementById("imagemDaForca").src = "./data/images/Forca-0.png";
+                        break;
+                    case 1:
+                        document.getElementById("imagemDaForca").src = "./data/images/Forca-1.png";
+                        break;
+                    case 2:
+                        document.getElementById("imagemDaForca").src = "./data/images/Forca-2.png";
+                        break;
+                    case 3:
+                        document.getElementById("imagemDaForca").src = "./data/images/Forca-3.png";
+                        break;
+                    case 4:
+                        document.getElementById("imagemDaForca").src = "./data/images/Forca-4.png";
+                        break;
+                    case 5:
+                        document.getElementById("imagemDaForca").src = "./data/images/Forca-5.png";
+                        document.getElementById("palavraSorteadaText").innerText = "Você perdeu"
+                        document.getElementById("botaoSorteio").innerText = "Tente Novamente";
+                        document.getElementById("botaoSorteio").disabled = false;
+                        break;
                 }
                 break
             }
-            
-        } 
+
+        }
     }
-    else
-    {
-        document.getElementById("palavraSorteadaText").innerText = "Sorteie uma palavra estupido"
+    else {
+        document.getElementById("palavraSorteadaText").innerText = "Você deve sortear uma palavra antes de começar a jogar"
     }
-    
+
 } 
