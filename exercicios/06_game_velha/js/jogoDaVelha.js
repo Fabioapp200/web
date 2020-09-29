f091f32edd9e865b6593f54940eb2153f70bfd1c
andre <andredessunte@gmail.com>
2020-09-29 20:49:58 -0300

    code

---
+++ exercicios/06_game_velha/js/jogoDaVelha.js
@@ -0,0 +1,27 @@
+var jogador;
+var jogador2;
+
+function inicioJogadorAleatorio()
+{
+    var jogadorSorteado = Math.floor(Math.random() * 2);
+
+}
+
+
+function trocarTxt()
+{
+    switch ()
+    {
+        
+        case 0:
+            document.getElementById("").innerText = "x";
+                break;
+        case 1:
+            document.getElementById("").innerText = "O";
+                break;
+        case 2:
+            document.getElementById("").innerText = "";
+                break;
+
+    }
+}
