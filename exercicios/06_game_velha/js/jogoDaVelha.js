6bde1e18e350e8ab8540d0da6b8687c884792adf
Fabio <pintofabio100@gmail.com>
2020-09-29 21:51:58 -0300

    monkeys jumping on the bed

--- exercicios/06_game_velha/js/jogoDaVelha.js
+++ exercicios/06_game_velha/js/jogoDaVelha.js
@@ -1,57 +1,56 @@
 var vezP1;
+var casa=[];
 
-function jogar(numeroDaCasa)
-{
-    console.log(numeroDaCasa);
-    document.getElementById(numeroDaCasa).innerText=("Y");
-    if(vezP1)
-    {
-        document.getElementById(numeroDaCasa).innerText=("X");
-        vezP1=0;
+function jogar(numeroDaCasa) {
+    document.getElementById(numeroDaCasa).innerText = ("Y");
+    if (vezP1) {
+        document.getElementById(numeroDaCasa).innerText = ("X");
+        document.getElementById(numeroDaCasa).disabled = true;
+        casa[numeroDaCasa - 1] = "X";
+        console.log(casa);
+        vezP1 = 0;
     }
-    else
-    {
-        document.getElementById(numeroDaCasa).innerText=("O");
-        vezP1=1;
+    else {
+        document.getElementById(numeroDaCasa).innerText = ("O");
+        document.getElementById(numeroDaCasa).disabled = true;
+        casa[numeroDaCasa - 1] = "O";
+        console.log(casa);
+        vezP1 = 1;
+    }
+    if((casa[0] && casa[1] && casa[2]) == "X"){
+        console.log("vitoria");
     }
 }
 
-function inicioJogadorAleatorio()
-{
+function inicioJogadorAleatorio() {
     var jogadorSorteado = Math.floor(Math.random() * 2);
 
 }
-function jogarNovamente()
-{
-    for(i=0;i<9;i++)
-    {
-        numeroDaCasa=i;
-        document.getElementById(numeroDaCasa).innerText=("u");
+function jogarNovamente() {
+    
+    
+    for (i = 0; i < 9; i++) {
+        numeroDaCasa = i;
+        document.getElementById(numeroDaCasa + 1).innerHTML = "&nbsp";
+        document.getElementById(numeroDaCasa + 1).disabled = false;
+        document.getElementById(2).disabled = false;
     }
 }
 
 
-function trocarTxt()
-{
-   /*  switch ()
-    {
-        
-        case 0:
-            document.getElementById("").innerText = "x";
-                break;
-        case 1:
-            document.getElementById("").innerText = "O";
-                break;
-        case 2:
-            document.getElementById("").innerText = "";
-                break;
-
-    } */
+function trocarTxt() {
+    /*  switch ()
+     {
+         
+         case 0:
+             document.getElementById("").innerText = "x";
+                 break;
+         case 1:
+             document.getElementById("").innerText = "O";
+                 break;
+         case 2:
+             document.getElementById("").innerText = "";
+                 break;
+ 
+     } */
 }
-
-/* function vitoria()
-{
-    if(document.getElementById("1"&&"2"&&"3")||("4"&&"5"&&"6")||("7"&&"8"&&"9")||
-                            ("1"&&"4"&&"7")||("2"&&"5"&&"8")||("3"&&"6"&&"9")||
-                            ("1"&&"5"&&"9")||("3"&&"5"&&"7")||)
-} */
